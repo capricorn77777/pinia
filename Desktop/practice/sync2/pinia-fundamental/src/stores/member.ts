81d105d218d6f62df65a08ff1decf5121ddd48b7
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-19 20:51:45 +0900

    member.tsにaddMember追記

--- Desktop/practice/sync2/pinia-fundamental/src/stores/member.ts
+++ Desktop/practice/sync2/pinia-fundamental/src/stores/member.ts
@@ -26,6 +26,9 @@ export const useMembersStore = defineStore({
         points: 35, note: "初回入会特典あり。"});
         this.memberList.set(47783,{id: 47783, name: "鈴木二郎", email: "mue@example.com",
         points: 53});
+        },
+        addMember(member: Member): void {
+            this.memberList.set(member.id, member);
         }
     }
 })
