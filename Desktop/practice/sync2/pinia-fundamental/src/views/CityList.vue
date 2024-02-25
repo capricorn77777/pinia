c8b4fc14e79028cb836a8d3ad906bd475d82091d
Isikawa Shotaro <zhengtailangs7@gmail.com>
2024-02-26 08:00:13 +0900

    CityList.vueをviewに追加

---
+++ Desktop/practice/sync2/pinia-fundamental/src/views/CityList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { VueElement } from 'vue';
+import {computed} from 'vue';
+import {RouterLink} from 'vue-router';
+import {useWeatherSrore} from "@/stores/weather";
+import type {City} from "@/stores/weather";
+
+const weatherStore = useWeatherStore();
+weatherStore.prepareCityList();
+
+const cityList = computed(
+    (): Map<string, City> => {
+        return weatherStore.cityList;
+    }
+);
+</script>
+
+<template>
+    <ul>
+        <li
+            v-for="[id, city] in cityList"
+            v-vind:key="id">
+        <RouterLink v-bind:to="{name: 'weatherInfo', params: {id: id}}"></RouterLink>
+        </li>
+    </ul>
+</template>
